eece40f4b5368151d8bec048066f8e5d16c9b0c4
Aaron Armstrong <aaronarmstrong1490@gmail.com>
2024-01-08 08:19:38 +0000

    added some extra validation for password generator but haven't got it to work yet (work in progress)

--- script.js
+++ script.js
@@ -10,18 +10,43 @@ var lowerCasedCharacters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', '
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
 
-// Function to prompt user for password options
+// Define min and max password lengths
+const minLen = 8;
+const maxLen = 128;
+
+
+// Function to prompt user for password length and options
 function getPasswordOptions() {
   var length = prompt("What password length would you like? (Must be between 8-128 characters)") // prompts user for a number of characters in password, then stores this in var length
- if (length < 8 || length > 128) { // password length must be between 8-128 characters in length, otherwise user will get returned an error stating password length has to fit these parameters
+ if (length < minLen || length > maxLen) { // password length must be between 8-128 characters in length, otherwise user will get returned an error stating password length has to fit these parameters
   alert("Password length has to be 8-128 characters in length!");
   return null
-} else if (length.typeof !== Number) { // if user inputs anything other than a number, then they will be returned an error stating that a number is required
+} else if (isNaN(length)) { // if user inputs anything other than a number, then they will be returned an error stating that a number is required
   alert("Please type a number!");
 } 
+// while (isNaN(length) && length < minLen && length > maxLen) {
+//   var upperCase = confirm("Would you like to include upper case characters in your password?")
+//   var lowerCase = confirm("Would you like to include lower case characters in your password?")
+//   var number = confirm("Would you like to include numbers in your password?")
+//   var special = confirm("Would you like to include special characters in your password?")
 }
+// }
 
 getPasswordOptions() // call the getPasswordOptions function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Function for getting a random element from an array
